8ec007540331a33b95e7b80a538ce9dd8d22959d
Natasha Libera <tash@artoftash.com>
2026-06-17 14:46:02 +0200

    Created an extremely basic widget in which to test scanning for Bluetooth devices.

--- messenger/widgets.py
+++ messenger/widgets.py
@@ -72,7 +72,7 @@ class DebugMessages(BoxLayout):
             size_hint_y=None,
             halign='left',
             valign='middle',
-            text_size=(280, None)
+            text_size=(280, None),
         )
         label.bind(
             texture_size=lambda inst, size: setattr(inst, 'height', size[1] + 10)
@@ -88,4 +88,40 @@ class DebugMessages(BoxLayout):
 
         print('Send Message - not yet implemented.')
 
-# class DebugBluetoothClient(BoxLayout):
+class DebugBluetoothDevices(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(DebugBluetoothDevices, self).__init__(**kwargs)
+
+        self.orientation = 'vertical'
+        self.scroll = ScrollView()
+        self.devices_container = GridLayout(
+            cols=1,
+            spacing=4,
+            size_hint_y=None,
+        )
+        self.devices_container.bind(minimum_height=self.devices_container.setter('height'))
+        self.scroll.add_widget(self.devices_container)
+        self.add_widget(self.scroll)
+
+        self.check_button = Button(
+            text='Refresh Device List',
+            size_hint_y=None,
+            height=80
+        )
+        self.check_button.bind(on_press=self.on_check_devices_button_pressed)
+        self.add_widget(self.check_button)
+
+    def update_devices(self):
+        devices = ['Device 1', 'Device 2', 'Device 3'] # temp placeholder
+        for device in devices:
+            self.devices_container.add_widget(Label(
+                text=device,
+                size_hint_y=None,
+                halign='left',
+                valign='middle',
+                text_size=(280, None),
+            ))
+
+    def on_check_devices_button_pressed(self, button_instance):
+        self.update_devices()

ca4b0053ff300da764a1aaf62293413d06c6830b
Natasha Libera <tash@artoftash.com>
2026-06-24 09:48:09 +0200

    Tweated Debug Messages layout.

--- messenger/widgets/debug/messages.py
+++ messenger/widgets/debug/messages.py
@@ -1,7 +1,7 @@
-from kivy.uix.scrollview import ScrollView
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.textinput import TextInput
 from .components.debug_layout import DebugLayout
 from ..utils import fit_height
@@ -12,15 +12,21 @@ class DebugMessages(DebugLayout):
         super(DebugMessages, self).__init__(**kwargs)
 
         # Top-level page container
-        self.container = BoxLayout(orientation='vertical')
+        self.container = BoxLayout(orientation='vertical', padding=10, spacing=10)
         self.add_widget(self.container)
 
         # Chat Room Loader
-        self.chat_room_loader = BoxLayout(size_hint_y=None, height=50)
+        self.chat_room_loader = BoxLayout(size_hint_y=None, height=30, spacing=10)
         self.container.add_widget(self.chat_room_loader)
+        self.chat_room_loader_label = Label(text='Chat room:', size_hint_x=.3)
+        self.chat_room_loader_input = TextInput(size_hint_x=.2)
+        self.chat_room_loader_button = Button(text='Load', size_hint_x=.4)
+        self.chat_room_loader.add_widget(self.chat_room_loader_label)
+        self.chat_room_loader.add_widget(self.chat_room_loader_input)
+        self.chat_room_loader.add_widget(self.chat_room_loader_button)
 
         # Refresh Button
-        self.refresh_button = Button(size_hint_y=None, height=50)
+        self.refresh_button = Button(text='Refresh Messages', size_hint_y=None, height=50)
         self.container.add_widget(self.refresh_button)
 
         # Scrolling Container
@@ -33,7 +39,7 @@ class DebugMessages(DebugLayout):
         self.container.add_widget(self.message_container)
 
         # Input Form
-        self.form = BoxLayout(size_hint_y=None, height=80)
+        self.form = BoxLayout(size_hint_y=None, height=80, spacing=10)
         self.container.add_widget(self.form)
         self.text_input = TextInput(multiline=True, size_hint_x=.8)
         self.submit_button = Button(text='Send', size_hint_x=.2)

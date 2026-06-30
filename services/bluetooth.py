36be3ad0da96c69db5ee8dc694e8fc71604aeee8
Natasha Libera <tash@artoftash.com>
2026-06-30 21:18:44 +0200

    Implemented turn_discoverability_on function.

--- services/bluetooth.py
+++ services/bluetooth.py
@@ -1,2 +1,12 @@
+from jnius import autoclass
+
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+Intent = autoclass('android.content.Intent')
+BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
+
 def turn_discoverability_on():
-    print('Want to turn the device\'s Bluetooth discovery mode to \'on\' or \'visible\'.')
+    activity = PythonActivity.mActivity
+    intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+    intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+
+    activity.startActivity(intent)

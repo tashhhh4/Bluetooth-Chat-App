15af68bcae9fd41422c0a59b1b6478349bb64625
Natasha Libera <tash@artoftash.com>
2026-06-30 17:54:57 +0200

    Committed current state.

--- messenger/widgets/debug/transport.py
+++ messenger/widgets/debug/transport.py
@@ -12,15 +12,52 @@ from config import SERVICE_UUID
 from config import ENVIRONMENT
 if ENVIRONMENT == 'debug':
     from pprint import pprint
-    import socket
-    import jnius
+    import threading
     from jnius import autoclass
     JavaUUID = autoclass('java.util.UUID')
     java_uuid = JavaUUID.fromString(str(SERVICE_UUID))
     BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
     adapter = BluetoothAdapter.getDefaultAdapter()
     server = adapter.listenUsingRfcommWithServiceRecord('Blu2', java_uuid)
-    pprint(dir(server))
+
+    def listen():
+        conn = server.accept()
+        print('Created a connection. Connection is:', conn)
+        print('type:', type(conn))
+        print('dir:')
+        pprint(dir(conn))
+
+    def connect():
+        print('Running connect.')
+        device = adapter.getRemoteDevice('AA:BB:CC:DD:EE:FF')
+        print('About the device...')
+        print('ACCESS_ALLOWED:', device.ACCESS_ALLOWED)
+        print('ACCESS_REJECTED:', device.ACCESS_REJECTED)
+        print('ACCESS_UNKNOWN:', device.ACCESS_UNKNOWN)
+        print('CONNECTION_ACCESS_NO:', device.CONNECTION_ACCESS_NO)
+        print('CONNECTION_ACCESS_YES:', device.CONNECTION_ACCESS_YES)
+        print('CREATOR:', device.CREATOR)
+        print('DEVICE_TYPE_HEADSET:', device.DEVICE_TYPE_HEADSET)
+        print('DEVICE_TYPE_UNKNOWN:', device.DEVICE_TYPE_UNKNOWN)
+        print('ERROR:', device.ERROR)
+        print('METADATA_DEVICE_TYPE:', device.METADATA_DEVICE_TYPE)
+        print('PAIRING_VARIANT_CONSENT:', device.PAIRING_VARIANT_CONSENT)
+        print('BOND_BONDED:', device.BOND_BONDED)
+        print('BOND_BONDING:', device.BOND_BONDING)
+        print('BOND_LOSS_REASON_BREDR_AUTH_FAILURE:', device.BOND_LOSS_REASON_BREDR_AUTH_FAILURE)
+        print('BOND_LOSS_REASON_BREDR_INCOMING_PAIRING:', device.BOND_LOSS_REASON_BREDR_INCOMING_PAIRING)
+        print('BOND_LOSS_REASON_LE_ENCRYPT_FAILURE:', device.BOND_LOSS_REASON_LE_ENCRYPT_FAILURE)
+        print('BOND_LOSS_REASON_LE_INCOMING_PAIRING:', device.BOND_LOSS_REASON_LE_INCOMING_PAIRING)
+        print('BOND_LOSS_REASON_UNKNOWN:', device.BOND_LOSS_REASON_UNKNOWN)
+        print('BOND_NONE:', device.BOND_NONE)
+        print('bond state:', device.getBondState())
+        print('is connected:', device.isConnected())
+        print('name:', device.name)
+
+
+    def start_thread():
+        threading.Thread(target=listen, daemon=True).start()
+        print('Started thread.')
 
     def send(address, message):
         pass
@@ -28,6 +65,9 @@ if ENVIRONMENT == 'debug':
     def recv():
         pass
 
+    # Try it out
+    start_thread()
+    connect()
 
 class DebugTransport(DebugLayout):
     def __init__(self, **kwargs):

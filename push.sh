8b3796a88e902234a4089a3288f666a9524017a0
Natasha Libera <tash@artoftash.com>
2026-07-02 12:21:58 +0200

    Created adb push script. - Individual Python files can now be replaced without rebuilding the entire project.

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/fish
+
+set FILE $argv[1]
+
+if test -z "$FILE"
+    echo "Usage: push <path/to/file>"
+    exit 1
+end
+
+adb push $FILE /data/local/tmp/$FILE; or exit 1
+adb shell run-as com.blu2chat.blu2chat cp /data/local/tmp/$FILE files/app/$FILE; or exit 1
+echo "Pushed $FILE."
+adb shell rm /data/local/tmp/$FILE
+echo "Cleaned up tmp dir."

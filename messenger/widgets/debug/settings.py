1a51d4556fe7fd4c2b18e9b8c555e69f91a8ebc0
Natasha Libera <tash@artoftash.com>
2026-07-10 18:50:21 +0200

    Polished the DebugSettings View.

--- messenger/widgets/debug/settings.py
+++ messenger/widgets/debug/settings.py
@@ -1,119 +1,120 @@
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from messenger.widgets.utils import bind_height_to_content_height, wrap_text
 from .components.debug_layout import DebugLayout
 import config
 from db.manager import settings
 
-class ServiceIDCell(BoxLayout):
+class UuidCell(BoxLayout):
 
-    def __init__(self, **kwargs):
-        super(ServiceIDCell, self).__init__(**kwargs)
+    value = StringProperty('')
 
-        self.label = Label(text='Print results here')
-        self.label.bind(width=lambda ins, val: setattr(ins, 'text_size', (val, None)))
+    def __init__(self, value, **kwargs):
+
+        self.value = value
+
+        super(UuidCell, self).__init__(**kwargs)
+
+        self.label = Label(text=self.value)
+        wrap_text(self.label)
         self.add_widget(self.label)
 
-        self.run_settings_test()
-
-    def run_settings_test(self):
-        service_uuid = config.SERVICE_UUID
-        self.label.text = f'SERVICE UUID: {service_uuid}'
-
-
-class DeviceIDCell(BoxLayout):
-
-    def __init__(self, **kwargs):
-        super(DeviceIDCell, self).__init__(**kwargs)
-
-        self.label = Label(text='Print results here')
-        self.label.bind(width=lambda ins, val: setattr(ins, 'text_size', (val, None)))
-        self.add_widget(self.label)
-
-        self.run_get_device_id()
-
-    def run_get_device_id(self):
-        device_id = settings.get_device_uuid()
-        self.label.text = f'DEVICE_UUID: {device_id}'
-
-
-class SettingsList(BoxLayout):
-    """ Show all the Settings that exist and allow deleting them for testing purposes. """
-
-    DELETABLE = ['SERVICE_UUID', 'DEVICE_UUID']
-
-    def __init__(self, **kwargs):
-        super(SettingsList, self).__init__(**kwargs)
-
-        self.orientation='vertical'
-
-        self.table = GridLayout(
-            cols=3,
-            size_hint_y=None,
-            row_default_height=dp(160),
-            row_force_default=True,
-            spacing=dp(5),
-            padding=dp(5),
-        )
-        self.table.bind(minimum_height=self.table.setter('height'))
-        self.add_widget(self.table)
-
-        self.add_header()
-        self.add_rows()
-
-    def add_header(self):
-        for heading in ['KEY', 'VALUE', 'OPTIONS']:
-            self.table.add_widget(Label(text=f'[b]{heading}[/b]', markup=True))
-
-    def add_rows(self):
-        app_settings = settings.list_all()
-
-        for setting in app_settings:
-            key_label = Label(
-                text=setting.key,
-                halign='left',
-                valign='middle',
-            )
-            value_label = Label(
-                text=setting.value,
-                halign='left',
-                valign='middle',
-            )
-            key_label.bind(width=lambda ins, val: setattr(ins, 'text_size', (val, None)))
-            value_label.bind(width=lambda ins, val: setattr(ins, 'text_size', (val, None)))
-            if setting.key in self.DELETABLE:
-                key = setting.key
-                def delete(_, key=key):
-                    settings.delete(key)
-                    self.table.clear_widgets()
-                    self.add_header()
-                    self.add_rows()
-                delete_button = Button(text='Delete')
-                delete_button.bind(on_press=delete)
-            else:
-                delete_button = Label(text='-')
-
-            self.table.add_widget(key_label)
-            self.table.add_widget(value_label)
-            self.table.add_widget(delete_button)
-
+    def on_value(self, _, value):
+        print('inside this UuidCell object. My value was changed. It should be', value)
+        self.label.text = value
 
 class DebugSettings(DebugLayout):
 
     def __init__(self, **kwargs):
         super(DebugSettings, self).__init__(**kwargs)
 
-        self.orientation = 'vertical'
-        self.spacing = dp(5)
-        self.padding = dp(5)
+        # Top-level Container
+        self.container = BoxLayout(orientation='vertical', spacing=dp(10), padding=dp(10))
+        self.add_widget(self.container)
 
-        settings_list = SettingsList()
-        settings_list.size_hint_y = 0.3
-        self.add_widget(settings_list)
+        # Refresh Button
+        self.refresh_button = Button(text='Refresh Page', size_hint_y=None, height=dp(50))
+        self.container.add_widget(self.refresh_button)
 
-        self.row_1 = BoxLayout(spacing=dp(5), padding=dp(5), height=dp(200))
-        self.add_widget(self.row_1)
-        self.row_1.add_widget(ServiceIDCell(size_hint_x=0.5))
-        self.row_1.add_widget(DeviceIDCell(size_hint_x=0.5))
+        # Config Chart Title
+        self.config_chart_title = Label(text='Settings Loaded from Config', size_hint_y=None, height=dp(30))
+        self.container.add_widget(self.config_chart_title)
+
+        # Config Settings
+        self.config_settings_container = BoxLayout(orientation='vertical', spacing=dp(10))
+        bind_height_to_content_height(self.config_settings_container)
+        self.container.add_widget(self.config_settings_container)
+
+        # Header Row
+        self.config_row_0 = BoxLayout(spacing=dp(10), size_hint_y=None, height=dp(30))
+        self.config_settings_container.add_widget(self.config_row_0)
+        self.key_label = Label(text='KEY', size_hint_x=.3)
+        self.config_row_0.add_widget(self.key_label)
+        self.value_label = Label(text='VALUE')
+        self.config_row_0.add_widget(self.value_label)
+
+        # SERVICE_UUID
+        self.config_row_1 = BoxLayout(spacing=dp(10), size_hint_y=None, height=dp(50))
+        self.config_settings_container.add_widget(self.config_row_1)
+        self.service_uuid_label = Label(text='SERVICE UUID', size_hint_x=.3)
+        self.config_row_1.add_widget(self.service_uuid_label)
+        self.service_uuid_cell = UuidCell(value='loading')
+        self.config_row_1.add_widget(self.service_uuid_cell)
+
+        # Settings Stored in Database
+        self.db_settings_container = BoxLayout(orientation='vertical', spacing=dp(10))
+        bind_height_to_content_height(self.db_settings_container)
+        self.container.add_widget(self.db_settings_container)
+
+        # Header Row
+        self.db_row_0 = BoxLayout(spacing=dp(10), size_hint_y=None, height=dp(30))
+        self.db_settings_container.add_widget(self.db_row_0)
+        self.db_key_label = Label(text='KEY', size_hint_x=.3)
+        self.db_row_0.add_widget(self.db_key_label)
+        self.db_value_label = Label(text='VALUE')
+        self.db_row_0.add_widget(self.db_value_label)
+        self.db_empty_widget = Widget(size_hint_x=.2)
+        self.db_row_0.add_widget(self.db_empty_widget)
+
+
+        # DEVICE_UUID
+        self.db_row_1 = BoxLayout(spacing=dp(10), size_hint_y=None, height=dp(50))
+        self.db_settings_container.add_widget(self.db_row_1)
+        self.device_uuid_key = Label(text='DEVICE_UUID', size_hint_x=.3)
+        self.db_row_1.add_widget(self.device_uuid_key)
+        self.device_uuid_cell = UuidCell(value='loading')
+        self.db_row_1.add_widget(self.device_uuid_cell)
+        self.device_uuid_x_button = Button(text='Delete', size_hint_x=.2)
+        self.db_row_1.add_widget(self.device_uuid_x_button)
+
+        # Spacer
+
+        self.spacer = Widget()
+        self.container.add_widget(self.spacer)
+
+        ### Bind Action ###
+
+        # Refresh Page
+        def refresh_page(_):
+            self.load_settings()
+        self.refresh_button.bind(on_press=refresh_page)
+
+        # Delete Device UUID
+        def delete_device_uuid(_):
+            settings.delete('DEVICE_UUID')
+            self.load_settings()
+        self.device_uuid_x_button.bind(on_press=delete_device_uuid)
+
+    def load_settings(self):
+        service_uuid = str(config.SERVICE_UUID)
+        self.service_uuid_cell.value = service_uuid
+        device_uuid = settings.get_device_uuid()
+        print('Device UUID db gave me is', device_uuid)
+        self.device_uuid_cell.value = device_uuid
+
+    def on_pre_enter(self):
+        self.load_settings()

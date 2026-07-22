0c04dfb0628e261740bbba582177d37bc7714a0a
Natasha Libera <tash@artoftash.com>
2026-07-22 16:35:57 +0200

    Tried various implementations of the bluetooth socket for Linux Desktop.

--- services/bluetooth_desktop.py
+++ services/bluetooth_desktop.py
@@ -1,8 +1,16 @@
 import socket
 import subprocess
 import logging
+from PySide6.QtBluetooth import (
+    QBluetoothAddress,
+    QBluetoothSocket,
+    QBluetoothServer,
+    QBluetoothServiceInfo,
+    QBluetoothUuid,
+)
 from services.bluetooth import BluetoothService
-from utils import accept_on_thread
+from config import SERVICE_UUID
+#from utils import accept_on_thread_qt
 
 def run_command(*args):
     return subprocess.run([*args], capture_output=True, text=True, check=True).stdout
@@ -29,19 +37,6 @@ class DesktopBluetoothService(BluetoothService):
                 break
         return address
 
-    def listen_for_connections(self):
-        logging.warning('Listen for connections - not yet implemented in DesktopBluetoothService')
-
-        print(self.whats_my_mac_address())
-
-        sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-
-        # accept_on_thread(
-        #     sock,
-        #     name='Connection Listener',
-        #     on_connected=self._handle_connection,
-        # )
-
     def load_paired_devices(self):
 
         # run bluetoothctl devices
@@ -63,6 +58,35 @@ class DesktopBluetoothService(BluetoothService):
 
         self.event_registry.emit_event('BONDED_DEVICES_UPDATED', devices)
 
+    def listen_for_connections(self):
+        logging.warning('Listen for connections - not yet implemented in DesktopBluetoothService')
+
+        address = self.whats_my_mac_address()
+        logging.info(f'My MAC address is {address}')
+
+        #
+        # sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        # sock.bind((address, 0))
+
+        # accept_on_thread_qt(
+        #     sock,
+        #     name='Connection Listener',
+        #     on_connected=self._handle_connection,
+        # )
+        #
+        # rfcommServer = QBluetoothServer(QBluetoothServiceInfo.RfcommProtocol)
+        # rfcommServer.newConnection.connect(self, QOverload<>.of(ChatServer.clientConnected))
+
+    def connect_to_device(self, address):
+        sock = QBluetoothSocket(QBluetoothServiceInfo.RfcommProtocol)
+        logging.warning(f'Created socket. The rest of the connect to device function is not yet implemented.')
+        address_obj = QBluetoothAddress(address)
+        logging.info('Created address_obj:', address_obj)
+        # uuid_obj = QBluetoothUuid.ServiceClassUuid(str(SERVICE_UUID))
+        # uuid_obj = QBluetoothUuid.ProtocolUuid(str(SERVICE_UUID))
+        # uuid_obj = QBluetoothUuid.StringFormat(str(SERVICE_UUID))
+
+        sock.connectToService(address_obj, str(SERVICE_UUID))
+
     def _handle_connection(self, sock):
-        print('inside _handle_Connection(desktop). sock is', sock)
         self.event_registry.emit_event('CONNECTION_ESTABLISHED', sock)
